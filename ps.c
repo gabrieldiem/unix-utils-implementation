4da3303ecf5e5e97524342446668136eb06a17b8
gabrieldiem <gabriel.diem5@gmail.com>
2024-09-09 16:24:19 -0300

    feat: Added comments to ps. Added missing error checking for open syscall

--- ps.c
+++ ps.c
@@ -18,7 +18,7 @@ static const char PROC_DIR_ABS_PATH[] = "/proc";
 static const char COMM_FILEPATH_RELATIVE_TO_PID[] = "comm";
 static const char LINE_BREAK = '\n', STRING_NULL_TERMINATOR = '\0';
 
-static const int CLOSE_DIR_ERROR_CODE = -1;
+static const int GENERIC_ERROR_CODE = -1;
 static const int IS_DIGIT_TRUE = 0;
 static const int SUCCESS = 0, FAILED = -1;
 
@@ -27,18 +27,26 @@ typedef struct process {
 	char *cmd_name;
 } process_t;
 
-
+/*
+ * Close `proc_directory`. If the closing fails the current process exits.
+ */
 void
 close_process_directory(DIR *proc_directory)
 {
 	int res = closedir(proc_directory);
 
-	if (res == CLOSE_DIR_ERROR_CODE) {
+	if (res == GENERIC_ERROR_CODE) {
 		perror("Error while closing process directory");
 		exit(EXIT_FAILURE);
 	}
 }
 
+/*
+ * Read an entity to the `entity` parameter from proc_directory.
+ * If the read finishes correctly `entity` is set to null.
+ * If the read fails `entity` is set to null, `proc_directory` gets closed
+ * and the current process exits.
+ */
 void
 read_entity_from_directory(DIR *proc_directory, struct dirent **entity)
 {
@@ -51,12 +59,20 @@ read_entity_from_directory(DIR *proc_directory, struct dirent **entity)
 	}
 }
 
+/*
+ * Load `pid` with the PID contained in `entity`.
+ */
 void
 load_pid(size_t *pid, struct dirent *entity)
 {
 	*pid = atoi(entity->d_name);
 }
 
+/*
+ * Create a heap-allocated string with the absolute filepath of the comm file in
+ * `comm_filepath` with the size saved in `comm_filepath_size`.
+ * If the memory allocation fails, `FAILED` is returned, otherwise `SUCCESS` is returned.
+ */
 int
 load_comm_filepath(char **comm_filepath, int *comm_filepath_size, size_t pid)
 {
@@ -81,11 +97,24 @@ load_comm_filepath(char **comm_filepath, int *comm_filepath_size, size_t pid)
 	return SUCCESS;
 }
 
+/*
+ * Save the text content of the comm file pointed by the string `comm_filepath`
+ * (with an summed null string terminator) into the newly heap-allocated
+ * `cmd_name` string.
+ * If the opening or the reading of `comm_filepath` fails, or the memory
+ * allocation for `cmd_name` fails, `FAILED` is returned and `cmd_name` is
+ * nulled, otherwise `SUCCESS` is returned.
+ */
 int
 read_comm_file(char **cmd_name, char *comm_filepath)
 {
-	int comm_fd = open(comm_filepath, O_RDONLY);
 	int position = 0;
+	int comm_fd = open(comm_filepath, O_RDONLY);
+	if (comm_fd == GENERIC_ERROR_CODE) {
+		perror("Failed to open comm file form path");
+		*cmd_name = NULL;
+		return FAILED;
+	}
 
 	char temp_buff[TEMP_BUFF_SIZE];
 	for (int i = 0; i < TEMP_BUFF_SIZE; i++) {
@@ -127,13 +156,26 @@ read_comm_file(char **cmd_name, char *comm_filepath)
 		return FAILED;
 	}
 
-	if (bytes_read != 0) {
+	if (bytes_read > 0) {
 		strcat(*cmd_name, temp_buff);
+	} else if (bytes_read == GENERIC_ERROR_CODE) {
+		perror("Failed to read comm file");
+		close(comm_fd);
+		free(*cmd_name);
+		*cmd_name = NULL;
+		return FAILED;
 	}
+
 	close(comm_fd);
 	return SUCCESS;
 }
 
+/*
+ * Load the text content of the comm file corresponding to the process with the
+ * PID `pid` into the newly heap-allocated `cmd_name` string. If there is an
+ * error processing the file of allocating memory, `FAILED` is returned and
+ * `cmd_name` is nulled, otherwise, `SUCCESS` is returned.
+ */
 int
 load_cmd_name(char **cmd_name, size_t pid)
 {
@@ -159,6 +201,11 @@ load_cmd_name(char **cmd_name, size_t pid)
 	return SUCCESS;
 }
 
+/*
+ * Determine if all the characters of `string` make up a number. A null string terminator is assumed
+ * to be at the end of `string`.
+ * Returns `true` if `string` is a number, returns `false` otherwise
+ */
 bool
 is_number(char *string)
 {
@@ -166,7 +213,7 @@ is_number(char *string)
 	int i = 0;
 	while (i < size) {
 		char c = string[i];
-		if (isdigit(c) == IS_DIGIT_TRUE || c == STRING_NULL_TERMINATOR) {
+		if (isdigit(c) == IS_DIGIT_TRUE) {
 			return false;
 		}
 		i++;
@@ -175,6 +222,10 @@ is_number(char *string)
 	return true;
 }
 
+/*
+ * Free the heap-allocated memory for each entry of the vector `processes` and for the vector itself.
+ * Afterwards, `processes` is nulled and `processes_size` is set to 0.
+ */
 void
 free_processes_vector(process_t **processes, size_t *processes_size)
 {
@@ -187,6 +238,13 @@ free_processes_vector(process_t **processes, size_t *processes_size)
 	*processes_size = 0;
 }
 
+/*
+ * Add a process_t entry to the heap-allocated vector `processes` of size
+ *`processes_size` from the information of `entity`. `processes`'s memory and
+ *`processes_size` will grow accordingly. If extracting the command from the
+ *comm file or allocating the memory for the expansion of `processes` fails,
+ *`FAILED` is returned, otherwise `SUCCESS` is returned
+ */
 int
 add_process(process_t **processes, size_t *processes_size, struct dirent *entity)
 {
@@ -224,34 +282,46 @@ add_process(process_t **processes, size_t *processes_size, struct dirent *entity
 	return SUCCESS;
 }
 
+/*
+ * The return value is 0 if `process1` and `process2` have the same PID, 1 is
+ * the PID of `process1` is greater than the PID of `process2`, or -1 otherwise.
+ */
 int
-process_t_comparator(const void *elem1, const void *elem2)
+process_t_comparator(const void *process1, const void *process2)
 {
-	process_t process1 = *((process_t *) elem1);
-	process_t process2 = *((process_t *) elem2);
+	process_t _process1 = *((process_t *) process1);
+	process_t _process2 = *((process_t *) process2);
 
-	if (process1.pid > process2.pid) {
+	if (_process1.pid > _process2.pid) {
 		return 1;
-	} else if (process1.pid < process2.pid) {
+	} else if (_process1.pid < _process2.pid) {
 		return -1;
 	} else {
 		return 0;
 	}
 }
 
+/*
+ * Sort the `processes` of size `processes_size` in ascending order by their PID.
+ */
 void
 sort_vector_by_pid(process_t *processes, size_t processes_size)
 {
 	qsort(processes, processes_size, sizeof(process_t), process_t_comparator);
 }
 
+/*
+ * Print the information of the process_t elements of `processes` of size
+ * `processes_size` with spacing depending on the digit count of the PID number,
+ * so the PID and the COMMAND are aligned with a center line.
+ */
 void
 print_processes(process_t *processes, size_t processes_size)
 {
 	int max_pid = processes[processes_size - 1].pid;
 	int max_spaces = snprintf(NULL, 0, "%d", max_pid);
 
-	for (int k = 0; k < max_spaces - 2; k++) {
+	for (int i = 0; i < max_spaces - 2; i++) {
 		printf(" ");
 	}
 	printf("PID COMMAND\n");
