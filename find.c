39be0f2fd9e561265a88106c8c692d39df9d19b6
gabrieldiem <gabriel.diem5@gmail.com>
2025-01-04 00:37:05 -0300

    fix: added flag to include strcasestr during linking

--- find.c
+++ find.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <linux/limits.h>
@@ -5,6 +6,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
